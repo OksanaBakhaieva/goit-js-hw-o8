719e923fe2cb77ce69a746a1da6fee0bf58a5f32
OksanaBakhaieva <obahaeva@gmail.com>
2023-12-29 04:07:02 +0000

    Changings for hw-08
    I've done my code in hw-07 by mistake. Here I've just pasted the code from the hw-07, and I will try to save my hw-07 without this code now.

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categories = document.querySelectorAll(".categories-item");
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach((item) => {
-    console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Category: ${item.lastElementChild.children.length}`);
-})
-
--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener(`input`, (e) => {
-    nameOutput.textContent = e.currentTarget.value.trim() ? e.currentTarget.value : `Anonymous`;
-})
